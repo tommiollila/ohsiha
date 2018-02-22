5be600098ebd4b94cc514e43d6723c41e4ddba50
Tommi Ollila <tommi.ollila@student.tut.fi>
2018-02-22 19:35:56 +0200

    Given e-mail by user is saved to the SQLite3-database.

--- first_app/views.py
+++ first_app/views.py
@@ -1,10 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .forms import EmailForm
+from .models import Person
 
 def index(request):
     if request.method == 'POST':
         form = EmailForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['your_email']
+            p = Person(email=email)
+            p.save()
+            return HttpResponseRedirect('/your-email/')
     else:
         form = EmailForm()
     return render(request, "first_app/index.html", {'form': form})
